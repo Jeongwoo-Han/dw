fe88903fb0c26a539f65e75ed26f5b5473cfc84b
kick4718@gmail.com <kick4718@gmail.com>
2020-10-13 09:51:12 +0000

    회사연혁 부분 카피 중

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,139 +1,357 @@
 
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-	<title>대웅제약</title>
-	
+<!DOCTYPE HTML><html lang="ko">
+	<head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=1920" />
+	<meta name="format-detection" content="telephone=no" />
+	<meta name="subject" content="삼진제약" id="metaSubject" />
+	<meta name="title" content="삼진제약" id="metaTitle" />
+	<meta name="keywords" content="삼진제약" id="metaKeywords" />
+	<meta name="description" content="삼진제약" id="metaDescription" />
+	<meta name="author" content="v-w" />
+	<meta name="copyright" content="삼진제약" />
+	<meta name="robots" content="index, follow" />
+	<title id="title">삼진제약</title>
 
-	<meta property="og:title" content="대웅제약">
-	<meta property="og:image" content="/resource/p/kr/image/main/daewoong_kakao.jpg"> 
-	<meta property="og:image:width" content="800"/>
-	<meta property="og:image:height" content="400"/> 
-	<meta property="og:url" content="https://www.daewoong.co.kr"/> 
-	<meta property="og:site_name" content="대웅제약"/> 
-	<meta property="og:type" content="daewoong.co.kr"/>
-
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-	<link rel="stylesheet" href="/css/swiper.min.css">
-	<link rel="stylesheet" href="/css/common.css">
-		<link rel="stylesheet" href="/css/main.css">
-		
-	<script src="/js/jquery-3.3.1.min.js"></script>
-	<script src="/js/ui.dw.js"></script>
-    <script src="/js/copy.js"></script>
-    <script src="/js/swiper.min.js"></script>
-    <script src="/js/gsap.min.js"></script>
-	<script>
-	</script>
 	
-</head>
+	<script src="/js/webfont.js"></script>
+	<link rel="stylesheet" href="/css/style.css" />
+	<link rel="stylesheet" href="/css/animation.css" />
+	<link rel="stylesheet" href="/css/base.css" />
+	
+	<script src="/js/jquery-3.3.1.min.js"></script>
+	<script src="/js/module-dist.js"></script>
 
-<body>
-<div class="wrap" >
-	<!----------------------- container // ----------------------->
-	<div id="container" class="container">
-		<!-- visual // -->
-		<div data-grid='{"case":"slide","option":{"effect":"fade","speed":0,"loop":true,"dotcircle":true,"autoplay":{"delay":5500,"disableOnInteraction":false}}}' class="swiper-container main-head">
-			<div class="swiper-wrapper">
-				<!-- item // -->
-                <div class="swiper-slide">
-                    <dl class="msg">
-                        <dt><span class="in">Greater Value For Your Life</span></dt>
-                        <dd>
-                            <span class="text-wrap"><span class="in">끊임없는 연구개발을 통해 고객의 건강한 삶을 위한 <br/>더 나은 가치를 만들어갑니다.</span></span>
-                            <a href="http://daewoong.crea-m.com/kr/rnd/laboratory" class="btn-more" target="_self">더 알아보기 <span class="ir i-go"></span></a>
-                        </dd>
-                    </dl>
-                    <span class="img"><span class="in" style="background-image:url(/images/main_slide_bg01.jpg);"></span></span>
-                </div>					
-                <div class="swiper-slide">
-                    <dl class="msg">
-                        <dt><span class="in">Daewoong around the world</span></dt>
-                        <dd>
-                            <span class="text-wrap"><span class="in">글로벌 현지 네트워크 구축을 통해 대한민국을 넘어 <br/>세계 속의 대웅제약으로 나아갑니다.</span></span>
-                            <a href="http://daewoong.crea-m.com/kr/company/global-network" class="btn-more" target="_self">더 알아보기 <span class="ir i-go"></span></a>
-                        </dd>
-                    </dl>
-                    <span class="img"><span class="in" style="background-image:url(/images/main_slide_bg02.jpg);"></span></span>
-                </div>					
-                <div class="swiper-slide">
-                    <dl class="msg">
-                        <dt><span class="in">Challenge, Smart work, Growth</span></dt>
-                        <dd>
-                            <span class="text-wrap"><span class="in">‘직원의 성장이 곧 기업의 성장’이라는 기업철학으로 자율적으로 일하고 몰입하여<br/>일할 수 있는 환경을 만들어가고 있습니다.</span></span>
-                            <a href="http://daewoong.crea-m.com/kr/company/culture" class="btn-more" target="_self">더 알아보기 <span class="ir i-go"></span></a>
-                        </dd>
-                    </dl>
-                    <span class="img"><span class="in" style="background-image:url(/images/main_slide_bg03.jpg);"></span></span>
-                </div>
-			</div>
-			<div class="swiper-pagination"></div>
-			<button type="button" class="swiper-button-prev"><span class="blind">이전보기</span></button>
-			<button type="button" class="swiper-button-next"><span class="blind">다음보기</span></button>
-		</div>
-		<!-- // visual -->
+	</head>
+	<body class="page-company">
+	
+		<div class="container submain-a no-translate js-inview-container">
+			<div class="wrap is-full">
+				<div class="background">
+					<div class="sizer"></div>
+					<div class="pic" style="background-image: url('/images/main_slide_bg01.jpg');"></div>
+				</div>
+				<div class="holder">
+					<div class="inner">
+						<div class="grid grid-a">
+							<div class="col hero">
+								<div class="table">
+									<div class="module">
 
-		<!-- contents // -->
-		<div class="contents" data-grid='{"case":"mainsc"}'>
+										<h1 class="title doc-title">
+											<strong>
+												<span class="js-lettering">회사연혁</span>
+											</strong>
+										</h1>
+										<p class="desc eng">
+											<span>Brief</span>
+											<span>History</span>
+										</p>
 
-			<!-- R&D // -->
-			<div class="rnd" data-grid='{"case":"aniclass","classname":"ani-visible","hook":0.5}'>
-				<h2 class="title" data-grid='{"case":"aniclass","classname":"ani-visible","hook":0.9}'>R&D</h2>
-				<p class="title-sub" data-grid='{"case":"aniclass","classname":"ani-visible","hook":0.9}'>대웅제약은 국제 수준의 생명공학 기술과<br />지속적인 연구개발을 통해 고부가가치 제품의 개발 및 생산에<br />주력하고 있습니다</p>
-				<a href="/kr/rnd/laboratory" class="btn-more" data-grid='{"case":"aniclass","classname":"ani-visible","hook":0.9}'>더 알아보기 <span class="ir i-go"></span></a>
-				<div data-grid='{"case":"slide","option":{"slidesPerView":1,"effect":"fade","speed":0,"loop":true,"preview":".preview","autoplay":{"delay":3500,"disableOnInteraction":false}}}' class="swiper-container list">
-					<div class="swiper-wrapper">
-						<!-- item // -->
-						<div class="swiper-slide" data-preview='<img src="/images/main_content_slide01.jpg" alt="" /> <span class="text">First/<br />Best in class<br />신약개발</span>'>
-							<span class="img"><span class="in"><img src="/images/main_content_slide01.png" alt="" /></span></span>
-							<dl class="text">
-								<dt>First/<br />Best in class<br />신약개발</dt>
-								<dd>
-									1. 글로벌 혁신신약<br />
-									2. First/Best in class 신약개발<br />
-									3. 오픈 콜라보레이션 통한 신규 파이프라인 확보
-								</dd>
-							</dl>
-						</div>
-						<!-- // item -->
-						<div class="swiper-slide" data-preview='<img src="/images/main_content_slide02.jpg" alt="" /> <span class="text">차세대<br />줄기세포치료제<br />개발</span>'>
-							<span class="img"><span class="in"><img src="/images/main_content_slide02.png" alt="" /></span></span>
-							<dl class="text">
-								<dt>차세대<br />줄기세포치료제<br />개발</dt>
-								<dd>
-									1. 스마트 줄기세포치료제 개발<br />
-									2. 나보타 Life cycle extension<br />
-									3. 인도네시아 바이오메카 구현
-								</dd>
-							</dl>
-						</div>
-						<div class="swiper-slide" data-preview='<img src="/images/main_content_slide03.jpg" alt="" /> <span class="text">글로벌 제품화</span>'>
-							<span class="img"><span class="in"><img src="/images/main_content_slide03.png" alt="" /></span></span>
-							<dl class="text">
-								<dt>글로벌 제품화</dt>
-								<dd>
-									1. 장기지속형 주사제 발매<br />
-									2. 개량신약(복합, 서방) 지속 발매<br />
-									3. 오픈 콜라보레이션을 통한 플랫폼 기술 확보
-								</dd>
-							</dl>
+									</div>
+								</div>
+							</div>
+							<div class="col list">
+								<div id="breadcrumb" class="breadcrumb">
+									<ul class="tab">
+										<li><a href="javascript:;"><i class="icon home"><em>Home</em></i></a></li>
+										<li><a href="javascript:;"><span>기업정보</span></a></li>
+										<li><strong>회사연혁</strong></li>
+									</ul>
+								</div>
+							</div>
 						</div>
 					</div>
-					<div class="swiper-pagination" data-type="fraction"></div>
-					<button type="button" class="swiper-button-prev"><span class="ir">이전보기</span><span class="preview"></span></button>
-					<button type="button" class="swiper-button-next"><span class="ir">다음보기</span><span class="preview"></span></button>
 				</div>
-				<span class="line-set">
-					<span class="line"></span><span class="line"></span><span class="line"></span><span class="line"></span><span class="line"></span><span class="line"></span><span class="line"></span><span class="line"></span><span class="line"></span><span class="line"></span><span class="line"></span>
-				</span>
 			</div>
-			<!-- // R&D -->
 		</div>
-		<!-- // contents -->
-	</div>
-	<!----------------------- // container ----------------------->
-</div>
-</body>
+
+		<div class="container section-slogan"><!-- section-slogan -->
+			<div class="wrap">
+				<p class="core">
+					삼진제약의 역사는 1968년 창업이래 반세기 동안<br />
+					제약업계의 명성을 지키며 우뚝 서 있습니다
+				</p>
+			</div>
+		</div><!-- // section-slogan -->
+
+		<div id="historySticky" class="container section-history-nav" style="margin-bottom:-120px;"><!-- section-history-nav -->
+			<div class="wrap">
+				<ul class="tab">
+					<li class="in"><a href="#year2011"><span>2020 - 2011</span></a></li>
+					<li><a href="#year2001"><span>2010 - 2001</span></a></li>
+					<li><a href="#year1997"><span>2000 - 1997</span></a></li>
+				</ul>
+				<div class="bar"><div></div></div>
+			</div>
+		</div><!-- // section-history-nav -->
+
+		<div id="timeline">
+
+			<div id="year2011" class="container section-history js-inview-container" data-group-idx="0" style="margin-top:180px;"><!-- section-history -->
+				<div class="wrap">
+					<div class="summary">
+						<div class="pic">
+							<img src="/images/history_header_2020.jpg" alt="" />
+						</div>
+						<p class="desc">
+							2020 - 2011
+						</p>
+					</div>
+					<div class="pos-relative">
+						<div class="year">
+							<div class="module">
+								<p class="start">2020</p>
+								<p class="end">2011</p>
+							</div>
+						</div>
+						<div class="lists">
+							<ul class="reset">
+								<li>
+									<p class="month">2020.04</p>
+									(주)Standigm사와 인공지능을 활용한 항암신약 공동연구 협약
+								</li>
+								<li>
+									<p class="month">2019.04</p>
+									한국화학연구원과 항암제 및 면역항암제 공동연구 협약
+								</li>
+								<li>
+									<p class="month">2019.03</p>
+									장홍순, 최용주 대표이사 사장 취임
+								</li>
+								<li>
+									<p class="month">2018.09</p>
+									압타바이오와 황반변성치료제 공동연구 협약
+								</li>
+								<li>
+									<p class="month">2017.12</p>
+									한양대학교 약학대학과 치매치료제 공동연구 협약
+								</li>
+								<li>
+									<p class="month">2017.09</p>
+									경구용 안구건조증치료제, 식품의약품안전처 임상 2상 승인
+								</li>
+								<li>
+									<p class="month">2017.02</p>
+									한국생명공학연구원과 치매 및 당뇨 치료제 공동연구 협약
+								</li>
+								<li>
+									<p class="month">2016.06</p>
+									한국파스퇴르와 간암 신약개발 공동연구 협약
+								</li>
+								<li>
+									<p class="month">2016.03</p>
+									압타바이오와 표적형항암제 신약개발 공동연구 협약
+								</li>
+								<li>
+									<p class="month">2015.12</p>
+									분자설계연구소와 신약개발 공동연구 협약
+								</li>
+								<li>
+									<p class="month">2015.06</p>
+									항혈전제 등 10품목 원료 및 제제기술 인도네시아 진출
+								</li>
+								<li>
+									<p class="month">2015.04</p>
+									경구용 안구건조증치료제, 식품의약품안전처 IND 승인
+								</li>
+								<li>
+									<p class="month">2014.10</p>
+									삼진제약㈜ 품질센터 개소
+								</li>
+								<li>
+									<p class="month">2014.10</p>
+									‘경구용 안구건조증치료제 SA001’신약개발 정부지원
+								</li>
+								<li>
+									<p class="month">2014.07</p>
+									미국 FDA, 삼진제약㈜ 여성용 에이즈 예방제 IND 승인
+								</li>
+								<li>
+									<p class="month">2013.09</p>
+									중앙연구소 판교 테크노밸리단지 개소
+								</li>
+								<li>
+									<p class="month">2013.03</p>
+									K.G.M.P 오송공장 준공
+								</li>
+								<li>
+									<p class="month">2012.06</p>
+									혁신형 제약기업 인증 (주관:보건복지부)
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div><!-- // section-history -->
+
+			<div id="year2001" class="container section-history js-inview-container" data-group-idx="1"><!-- section-history -->
+				<div class="motif-lines-holder">
+					<div class="motif-lines">
+						<div class="holder">
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+							<span></span>
+						</div>
+					</div>
+				</div>
+				<div class="wrap">
+					<div class="summary">
+						<div class="pic">
+							<img src="/images/history_header_2010.jpg" alt="" />
+						</div>
+						<p class="desc">
+							2010 - 2001
+						</p>
+					</div>
+					<div class="pos-relative">
+						<div class="year">
+							<div class="module">
+								<p class="start">2010</p>
+								<p class="end">2001</p>
+							</div>
+						</div>
+						<div class="lists">
+							<ul class="reset">
+								<li>
+									<p class="month">2010.11</p>
+									삼백만불 수출의 탑 수상 (제47회 무역의 날)
+								</li>
+								<li>
+									<p class="month">2008.11</p>
+									퇴직연금제 도입
+								</li>
+								<li>
+									<p class="month">2007.02</p>
+									인플루엔자 및 B형간염 치료신물질 특허출원
+								</li>
+								<li>
+									<p class="month">2006.09</p>
+									SARS균인 신종코로나바이러스 치료신물질 특허출원
+								</li>
+								<li>
+									<p class="month">2006.02</p>
+									C형간염 치료 신물질 특허출원
+								</li>
+								<li>
+									<p class="month">2005.05</p>
+									과기부 세포응용연구개발과제 선정
+								</li>
+								<li>
+									<p class="month">2005.01</p>
+									본사 사옥 준공 입주
+								</li>
+								<li>
+									<p class="month">2004.02</p>
+									한국경영자총협회 선정 제 16회 [보람의 일터] 대상 수상
+								</li>
+								<li>
+									<p class="month">2003.07</p>
+									본사 신규사옥 착공
+								</li>
+								<li>
+									<p class="month">2001.11</p>
+									백만불 수출의 탑 수상 (제38회 무역의 날)
+								</li>
+								<li>
+									<p class="month">2001.09</p>
+									이성우 대표이사 사장 취임
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div><!-- // section-history -->
+
+			<div id="year1997" class="container section-history js-inview-container" data-group-idx="2"><!-- section-history -->
+				<div class="wrap">
+					<div class="summary">
+						<div class="pic">
+							<img src="/images/history_header_2000.jpg" alt="" />
+						</div>
+						<p class="desc">
+							2000 - 1997
+						</p>
+					</div>
+					<div class="pos-relative">
+						<div class="year">
+							<div class="module">
+								<p class="start">2000</p>
+								<p class="end">1997</p>
+							</div>
+						</div>
+						<div class="lists">
+							<ul class="reset">
+								<li>
+									<p class="month">1999.</p>
+									'Consumer News' 주관 진통제부문 소비자 선호도 1위에 '게보린'선정
+								</li>
+								<li>
+									<p class="month">1999.07</p>
+									AIDS 치료제 미국특허취득, Y2K인증 (한국능률협회)
+								</li>
+								<li>
+									<p class="month">1998.12</p>
+									한미은행 TOP 10 기업 선정
+								</li>
+								<li>
+									<p class="month">1997.07</p>
+									은탑산업훈장 수훈 (중소기업부문 NO. 502호)
+								</li>
+								<li>
+									<p class="month">1996.05</p>
+									철탑산업훈장 수훈 (노사화합부문 NO. 1179호)
+								</li>
+								<li>
+									<p class="month">1996.01</p>
+									고형암치료제 국제특허출원 (국내 및 국외 45개국)
+								</li>
+								<li>
+									<p class="month">1993.05</p>
+									소염진통제 신물질 국제특허출원 (9개국)
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div><!-- // section-history -->
+
+		</div>
+
+		<div class="bottom-spacer" aria-hidden="true"></div>
+	</body>
+	
+	<script>
+		$(function(){
+		
+			var myApp = new BaseInView('#content .js-inview-container', {
+				countUpRestart: true, // 카운트업 재시작
+				onResize: function(base) {
+					// console.log(base.scope);
+				},
+				onScroll: function(base) {
+					// console.log(base);
+				}
+			});
+		
+		});
+	</script>
 </html>
